71f5a3235bdde0f12d9fa4a4df5c157c6092501f
youseock <mahwin7085@gmail.com>
2024-07-29 12:23:48 +0900

    chore: 디렉토리 구조 변경 및 prisma로 db 연결 확인

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  useEffect(() => {
+    fetch("/api/test").then((res) =>
+      res.json().then((data) => console.log(data, "??????"))
+    );
+    // fetch("/app/api/test").then((res) =>
+    //   res.json().then((data) => console.log(data, "??????"))
+    // );
+
+    // fetch("/src/app/api/test").then((res) =>
+    //   res.json().then((data) => console.log(data, "??????"))
+    // );
+    // fetch("/test").then((res) =>
+    //   res.json().then((data) => console.log(data, "??????"))
+    // );
+  }, []);
+  return <div>123</div>;
 }
